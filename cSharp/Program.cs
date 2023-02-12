ef77e31d47d38f23873b7b4fcb0689d285856bc0
Dmitry <doc9830@rambler.ru>
2023-02-12 17:20:37 +0300

    Исправление final

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -2,8 +2,8 @@
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 if (a>b){
-    Console.WriteLine("max = ",Convert.ToString(a));
+    Console.WriteLine("max = " + Convert.ToString(a));
 }else{
-    Console.WriteLine("max = ",Convert.ToString(b));
+    Console.WriteLine("max = " + Convert.ToString(b));
 }
 //Second task
