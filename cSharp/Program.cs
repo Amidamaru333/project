7a14bc8cd98034ae0fe2e105016091c6218620ba
Dmitry <doc9830@rambler.ru>
2023-02-12 17:15:09 +0300

    Исправление 1 задачи

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -2,7 +2,8 @@
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 if (a>b){
-    Console.WriteLine(Convert.ToString(a)+">"+Convert.ToString(b));
+    Console.WriteLine("max = ",Convert.ToString(a));
 }else{
-    Console.WriteLine(Convert.ToString(a)+"<"+Convert.ToString(b));
+    Console.WriteLine("max = ",Convert.ToString(b));
 }
+//Second task
