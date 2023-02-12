85c08791e28d76a6f47a18ffdbadc9de81002dc3
Dmitry <doc9830@rambler.ru>
2023-02-12 17:40:23 +0300

    Second task

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -1,9 +1,20 @@
-﻿//First task
+﻿//First task 
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a>b){
+//     Console.WriteLine("max = " + Convert.ToString(a));
+// }else{
+//     Console.WriteLine("max = " + Convert.ToString(b));
+// }
+//Second task
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-if (a>b){
-    Console.WriteLine("max = " + Convert.ToString(a));
-}else{
-    Console.WriteLine("max = " + Convert.ToString(b));
+int c = Convert.ToInt32(Console.ReadLine());
+if ((a>b)&(a>c)){
+    Console.WriteLine(Convert.ToString(a));
+}else if((b>c)&(b>a)){
+    Console.WriteLine(Convert.ToString(b));
 }
-//Second task
+else{
+    Console.WriteLine(Convert.ToString(c));
+}
