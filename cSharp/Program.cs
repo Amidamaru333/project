3f089529f1ae4cb0e60869fbfd9f7bb95a0bfb9a
georgeort <124184190+georgeort@users.noreply.github.com>
2023-02-12 16:47:45 +0300

    first commit

---
+++ cSharp/Program.cs
@@ -0,0 +1,8 @@
+﻿//First task
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b){
+    Console.WriteLine(Convert.ToString(a)+">"+Convert.ToString(b));
+}else{
+    Console.WriteLine(Convert.ToString(a)+"<"+Convert.ToString(b));
+}
